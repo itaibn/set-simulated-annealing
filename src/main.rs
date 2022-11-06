b758d2ae20ce6e2f196e40be11d77b908fbb5bb7
Itai Bar-Natan <itaibn3141@gmail.com>
2022-11-05 18:28:38 -0700

    Add simulated annealing

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ partition F_3^4 into affine lines.
 **/
 
 use std::collections::HashSet;
+use rand::prelude::*;
+use rand::distributions::Standard;
 use once_cell::sync::Lazy;
 
 const NUM_POINTS: usize = 81;
@@ -62,6 +64,40 @@ static LINE: Lazy<[[Point; 3]; NUM_LINES]> = Lazy::new(|| {
     line
 });
 
+#[test]
+fn test_line() {
+    for line in &*LINE {
+        for point in line {
+            assert!(point.0 < NUM_POINTS);
+        }
+        assert!(line[0] != line[1] && line[0] != line[2] && line[1] != line[2]);
+    }
+    for point0 in 0..NUM_POINTS {
+        for point1 in 0..NUM_POINTS {
+            if point0 == point1 {
+                continue;
+            }
+            let mut count = 0;
+            for line in &*LINE {
+                let mut has0 = false;
+                let mut has1 = false;
+                for p in line {
+                    if p.0 == point0 {
+                        has0 = true;
+                    }
+                    if p.0 == point1 {
+                        has1 = true;
+                    }
+                }
+                if has0 && has1 {
+                    count += 1;
+                }
+            }
+            assert!(count == 1);
+        }
+    }
+}
+
 /*
 impl std::ops::Index<usize> for Line {
     type Output = Point;
@@ -74,6 +110,12 @@ impl std::ops::Index<usize> for Line {
 impl Line {
     fn at(&self, index: usize) -> Point {
         (*LINE)[self.0][index]
+    }
+}
+
+impl Distribution<Line> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Line {
+        Line(rng.gen_range(0..NUM_LINES))
     }
 }
 
@@ -94,7 +136,21 @@ impl Default for Partition {
     }
 }
 
+impl Distribution<Partition> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Partition {
+        let mut partition = Partition::default();
+        for i in 0..PARTITION_SIZE {
+            partition.set(i, rng.gen());
+        }
+        partition
+    }
+}
+
 impl Partition {
+    fn count(&self, point: Point) -> &i32 {
+        &self.point_count[point.0]
+    }
+
     fn count_mut(&mut self, point: Point) -> &mut i32 {
         &mut self.point_count[point.0]
     }
@@ -106,11 +162,20 @@ impl Partition {
 
         let mut delta_cost = 0;
         for i in 0..3 {
-            let mut count = self.count_mut(line.at(i));
+            let count = self.count_mut(line.at(i));
             delta_cost += *count;
             *count += 1;
         }
         self.cost += delta_cost;
+        delta_cost
+    }
+
+    fn cost_at(&self, line: Line) -> i32 {
+        let mut delta_cost = 0;
+        for i in 0..3 {
+            let count = self.count(line.at(i));
+            delta_cost += *count;
+        }
         delta_cost
     }
 
@@ -121,49 +186,56 @@ impl Partition {
 
         let mut delta_cost = 0;
         for i in 0..3 {
-            let mut count = self.count_mut(line.at(i));
+            let count = self.count_mut(line.at(i));
             *count -= 1;
             delta_cost -= *count;
         }
         self.cost += delta_cost;
         delta_cost
     }
+
+    fn boltzmann_line<R: Rng + ?Sized>(&self, rng: &mut R, beta: f64) -> Line {
+        let mut z: f64 = 0.0;
+        for i in 0..NUM_LINES {
+            let line = Line(i);
+            z += f64::exp(-beta * self.cost_at(line) as f64);
+        }
+        let mut sample = rng.gen_range(0.0..z);
+        for i in 0..NUM_LINES {
+            let line = Line(i);
+            sample -= f64::exp(-beta * self.cost_at(line) as f64);
+            if sample <= 0.0 {
+                return line;
+            }
+        }
+        unreachable!()
+    }
+
+    fn step<R: Rng + ?Sized>(&mut self, rng: &mut R, beta: f64) -> i32 {
+        let idx = rng.gen_range(0..PARTITION_SIZE);
+        let unset = self.unset(idx);
+        unset + self.set(idx, self.boltzmann_line(rng, beta))
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
-}
+    const ITER: u32 = 8_000;
+    const MAX_BETA: f64 = 8.0;
+    const DEL_BETA: f64 = MAX_BETA / ITER as f64;
 
-#[test]
-fn test_line() {
-    for line in &*LINE {
-        for point in line {
-            assert!(*point < NUM_POINTS);
-        }
-        assert!(line[0] != line[1] && line[0] != line[2] && line[1] != line[2]);
-    }
-    for point0 in 0..NUM_POINTS {
-        for point1 in 0..NUM_POINTS {
-            if point0 == point1 {
-                continue;
-            }
-            let mut count = 0;
-            for line in &*LINE {
-                let mut has0 = false;
-                let mut has1 = false;
-                for p in line {
-                    if *p == point0 {
-                        has0 = true;
-                    }
-                    if *p == point1 {
-                        has1 = true;
-                    }
-                }
-                if has0 && has1 {
-                    count += 1;
-                }
-            }
-            assert!(count == 1);
+    let mut rng = thread_rng();
+    let mut beta: f64 = 0.0;
+    let mut log_z: f64 = PARTITION_SIZE as f64 * f64::ln(NUM_LINES as f64);
+    let mut partition: Partition = rng.gen();
+    for i in 0..ITER {
+        /*let del_cost = */
+            partition.step(&mut rng, beta);
+        beta += DEL_BETA;
+        log_z -= DEL_BETA * partition.cost as f64;
+        if i % 10 == 0 {
+            println!("Iter {}: beta {} cost {} log_z {}", i, beta,
+                partition.cost, log_z);
         }
     }
+    println!("z {:e}", f64::exp(log_z));
 }
